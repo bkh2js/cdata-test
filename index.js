913f634bfd41fd03cd58fe63355a31d7d80b604b
Bastian Khalil <bastian.khalil@readly.com>
2024-09-08 22:23:18 +0200

    update

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 const odbc = require('odbc');
 
-spreadsheetId = "1nqQYL36XNdS77SHF4HhaO0pNPEXajKGTWzX2nbw5cxk";
-apiKey = "AIzaSyB2zRIRjPq1_lpoE8-j1Sc5fy3L2Jf9Fm4";
-rkt = "5231444B4D5A303832333234333057454254523141310000000000000000000000000000000000004D6574726578696600005747475433323138305847370000";
+spreadsheetId = "";
+apiKey = "";
+RKT = "";
 
-const connectionString = `DRIVER={CDataDriverxx}`;
+verbosity=3
+log="/tmp/driverlog.log"
+const connectionString = `DRIVER={CDataDriverxx};RKT=${RKT};AuthScheme=Token;apiKey=${apiKey};LogFile=${log};Verbosity=${verbosity};spreadsheetId=${spreadsheetId}`;
 
 async function connectAndQuery(connectionString) {
-    await odbc.connect(connectionString, async (error, connection) => {
-        await connection.query('SELECT Type FROM initdata_Sheet1', (error, result) => {
-            if (error) { console.error(error) }
-            console.log(result);
-        });
-    });
+    try {
+        // Use the async/await version of the odbc.connect
+        const connection = await odbc.connect(connectionString);
+
+        console.log("Connected");
+        console.log(connection);
+
+        // Run the query and await the result
+        const result = await connection.query('SELECT Type FROM initdata_Sheet1');
+        
+        // Log the result
+        console.log(result);
+
+        // Close the connection after the query is done
+        await connection.close();
+        console.log("Connection closed");
+
+    } catch (error) {
+        // Handle any errors during the connection or query
+        console.error(error);
+    }
 }
 
 connectAndQuery(connectionString);
+
